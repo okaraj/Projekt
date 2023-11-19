0020a6722270a7df52cccf99a21035e826b810cc
orgka <orgka@ORGES-PC>
2023-11-19 17:06:35 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Title { get; set; }
+    public int Age { get; set; }
+    List<Employee> lista = new List<Employee>();
+    public List<Employee> CreateEmployee()
+    {
+        Console.WriteLine("Please enter the employees name: ");
+        Employee employee = new Employee();
+        Random random = new Random();
+        employee.FirstName = Console.ReadLine();
+        Console.WriteLine("Please enter the Last Name: ");
+        employee.LastName = Console.ReadLine();
+        Console.WriteLine("Please enter the employees age: ");
+        employee.Age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the employee's title");
+        employee.Title = Console.ReadLine();
+        employee.EmployeeID  = random.Next(0, 999999999);
+        lista.Add(employee);
+        return lista;
+    }
+
+    public List<Employee> ViewEmployees (List<Employee> lista)
+    {
+        return lista.Where(x => x.FirstName != null).ToList();
+        return lista.Where(x => x.LastName != null).ToList();
+        return lista.Where(x => x.Age != null).ToList();
+        return lista.Where (x => x.EmployeeID != null).ToList();
+        return lista.Where (x => x.Title != null).ToList();
+    }
+
+    public void DeleteEmployee(List<Employee> lista)
+    {
+        Console.WriteLine("Choose the Id of the employee that you want to delete. ");
+        int deleteId = Convert.ToInt32 (Console.ReadLine());
+        var employeeToDelete = lista.FirstOrDefault(x => x.EmployeeID == deleteId);
+        if (employeeToDelete != null)
+        {
+            lista.Remove(employeeToDelete);
+            Console.WriteLine("Employee with ID {0} has been deleted.", deleteId);
+        }
+        else
+        {
+            Console.WriteLine("No employee found with ID {0}.", deleteId);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+        List<Employee> listaPunonjesve = new List<Employee>();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Create Employee");
+            Console.WriteLine("2. View Employee");
+            Console.WriteLine("3. Delete Employee");
+            Console.WriteLine("4. Exit");
+            try
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        listaPunonjesve = employee.CreateEmployee();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("1. Per te shfaqur te gjithe punonjesit.");
+                        Console.WriteLine("2. Per te shfaqur te gjithe punonjersit me te medhenj se j18 vjec.");
+                        Console.WriteLine("3. Per te shfaqur te gjithe punonjesit me te vegjel ose 18 vjec.");
+                        try
+                        {
+                            int choice2 = Convert.ToInt32(Console.ReadLine());
+                            if (choice2 == 1)
+                            {
+                                Console.Clear();
+                                var a = employee.ViewEmployees(listaPunonjesve).Where(x => x != null).ToList();
+                                if (a.Any(x => x != null))
+                                {
+                                    foreach (var items in a)
+                                    {
+                                        Console.WriteLine();
+                                        Console.WriteLine($"Emri: {items.FirstName}");
+                                        Console.WriteLine($"Mbiemrii: {items.LastName}");
+                                        Console.WriteLine($"Mosha: {items.Age}");
+                                        Console.WriteLine($"titulli: {items.Title}");
+                                        Console.WriteLine($"Id: {items.EmployeeID}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You don't have an employee in this category");
+                                }
+                            }
+                            else if (choice2 == 2)
+                            {
+                                Console.Clear();
+                                var a = employee.ViewEmployees(listaPunonjesve).Where(x => x.Age > 18).ToList();
+                                if (a.Any(x => x != null))
+                                {
+                                    foreach (var items in a)
+                                    {
+                                        Console.WriteLine();
+                                        Console.WriteLine($"Emri: {items.FirstName}");
+                                        Console.WriteLine($"Mbiemrii: {items.LastName}");
+                                        Console.WriteLine($"Mosha: {items.Age}");
+                                        Console.WriteLine($"titulli: {items.Title}");
+                                        Console.WriteLine($"Id: {items.EmployeeID}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You don't have an employee in this category");
+                                }
+                            }
+                            else if (choice2 == 3)
+                            {
+                                Console.Clear();
+                                var a = employee.ViewEmployees(listaPunonjesve).Where(x => x.Age <= 18).ToList();
+                                if (a.Any(x => x != null))
+                                {
+                                    foreach (var items in a)
+                                    {
+                                        Console.WriteLine();
+                                        Console.WriteLine($"Emri: {items.FirstName}");
+                                        Console.WriteLine($"Mbiemrii: {items.LastName}");
+                                        Console.WriteLine($"Mosha: {items.Age}");
+                                        Console.WriteLine($"titulli: {items.Title}");
+                                        Console.WriteLine($"Id: {items.EmployeeID}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You don't have an employee in this category");
+                                }
+                            }
+                            else { Console.WriteLine("Please enter a valid number"); }
+                        }
+                        catch (Exception ex) { Console.WriteLine(ex.Message); }
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                       
+                            Console.Clear();
+                            employee.DeleteEmployee(listaPunonjesve);
+                            Console.ReadKey();
+                        
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
